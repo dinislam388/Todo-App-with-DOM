e482f25fa475cec459894b59e7cc140f22fd9429
dinislam388 <dinislam01319950749@gmail.com>
2023-12-05 22:31:11 +0600

    Done

---
+++ JavaScript.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addItem (){
+    
+    if(inputBox.value === ''){
+        alert("Write something!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "&#215";
+        li.appendChild(span)
+    }
+    inputBox.value = '';
+    saveData()
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData()
+}
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showItem(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+showItem()
